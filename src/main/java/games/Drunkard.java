d7ff7bf117a1ecdce5c83d12b66ac2bf03ac57a7
stoletnaedine <artur.islamgulov@gmail.com>
2019-04-04 21:45:24 +0300

    refactoring Drunkard

--- src/main/java/games/Drunkard.java
+++ src/main/java/games/Drunkard.java
@@ -1,9 +1,6 @@
 package games;
 
-import org.apache.commons.math3.util.MathArrays;
-
-import static games.CardUtils.CARDS_TOTAL_COUNT;
-import static games.CardUtils.PARS_TOTAL_COUNT;
+import static games.CardUtils.*;
 
 public class Drunkard {
 
@@ -37,28 +34,29 @@ public class Drunkard {
         return countCards;
     }
 
+    private static void moveCard(int playerIndex) {
+        playersCards[playerIndex][playersCardHeads[playerIndex]] = playersCards[playerIndex][playersCardTails[playerIndex]];
+        playersCardTails[playerIndex] = incrementIndex(playersCardTails[playerIndex]);
+        playersCardHeads[playerIndex] = incrementIndex(playersCardHeads[playerIndex]);
+    }
+
+    private static void addCard(int playerIndexTo, int playerIndexFrom) {
+        playersCards[playerIndexTo][playersCardHeads[playerIndexTo]] = playersCards[playerIndexFrom][playersCardTails[playerIndexFrom]];
+        playersCardTails[playerIndexFrom] = incrementIndex(playersCardTails[playerIndexFrom]);
+        playersCardHeads[playerIndexTo] = incrementIndex(playersCardHeads[playerIndexTo]);
+    }
+
+
     public static void main() {
 
-        int[] cards = new int[CARDS_TOTAL_COUNT];
+        int[] cards = getShaffledCards();
 
-        for (int i = 0; i < CARDS_TOTAL_COUNT; i++) {
-            cards[i] = i;
-        }
+        System.arraycopy(cards, 0, playersCards[0], 0, 18);
 
-        MathArrays.shuffle(cards);
-
-        for (int i = 0; i < CARDS_TOTAL_COUNT / 2; i++) {
-            playersCards[0][i] = cards[i];
-        }
-
-        for (int i = 0; i < CARDS_TOTAL_COUNT / 2; i++) {
-            playersCards[1][i] = cards[i + CARDS_TOTAL_COUNT / 2];
-        }
+        System.arraycopy(cards, 18, playersCards[1], 0, 18);
 
         playersCardHeads[0] = CARDS_TOTAL_COUNT / 2;
-        playersCardTails[0] = 0;
         playersCardHeads[1] = CARDS_TOTAL_COUNT / 2;
-        playersCardTails[1] = 0;
 
         int iteration = 0;
 
@@ -73,44 +71,31 @@ public class Drunkard {
             int movePos0 = playersCardTails[0];
             int movePos1 = playersCardTails[1];
 
-            String cardToString0 = CardUtils.toString(playersCards[0][movePos0]);
-            String cardToString1 = CardUtils.toString(playersCards[1][movePos1]);
-
             int cardValue0 = playersCards[0][movePos0] % PARS_TOTAL_COUNT;
             int cardValue1 = playersCards[1][movePos1] % PARS_TOTAL_COUNT;
 
             if (cardValue0 > cardValue1 ||
                     (cardValue0 == 0 && cardValue1 == 8)) {
                 result = "Выиграл игрок 1!";
-                playersCards[0][playersCardHeads[0]] = playersCards[0][playersCardTails[0]];
-                playersCardTails[0] = incrementIndex(playersCardTails[0]);
-                playersCardHeads[0] = incrementIndex(playersCardHeads[0]);
-                playersCards[0][playersCardHeads[0]] = playersCards[1][playersCardTails[1]];
-                playersCardTails[1] = incrementIndex(playersCardTails[1]);
-                playersCardHeads[0] = incrementIndex(playersCardHeads[0]);
+                moveCard(0);
+                addCard(0,1);
                 winner = true;
             }
-            else if (cardValue0 < cardValue1 ||
+            if (cardValue0 < cardValue1 ||
                     (cardValue0 == 8 && cardValue1 == 0)){
                 result = "Выиграл игрок 2!";
-                playersCards[1][playersCardHeads[1]] = playersCards[0][playersCardTails[1]];
-                playersCardTails[1] = incrementIndex(playersCardTails[1]);
-                playersCardHeads[1] = incrementIndex(playersCardHeads[1]);
-                playersCards[1][playersCardHeads[1]] = playersCards[0][playersCardTails[0]];
-                playersCardTails[0] = incrementIndex(playersCardTails[0]);
-                playersCardHeads[1] = incrementIndex(playersCardHeads[1]);
+                moveCard(1);
+                addCard(1,0);
                 winner = false;
             }
-            else if (cardValue0 == cardValue1) {
+            else {
                 result = "Спор — каждый остаётся при своих!";
-                playersCards[0][playersCardHeads[0]] = playersCards[0][playersCardTails[0]];
-                playersCardTails[0] = incrementIndex(playersCardTails[0]);
-                playersCardHeads[0] = incrementIndex(playersCardHeads[0]);
-
-                playersCards[1][playersCardHeads[1]] = playersCards[1][playersCardTails[1]];
-                playersCardTails[1] = incrementIndex(playersCardTails[1]);
-                playersCardHeads[1] = incrementIndex(playersCardHeads[1]);
+                moveCard(0);
+                moveCard(1);
             }
+
+            String cardToString0 = CardUtils.toString(playersCards[0][movePos0]);
+            String cardToString1 = CardUtils.toString(playersCards[1][movePos1]);
 
             System.out.printf("Итерация №%d: Игрок №1 карта: %s; игрок №2 карта: %s. \n" +
                                 result + "\n" +
