2cce9130cf6fb55fa01ca9a534b2dbf1a71ab66d
stoletnaedine <artur.islamgulov@gmail.com>
2019-04-04 14:54:54 +0300

    refactoring Choice

--- src/main/java/games/Choice.java
+++ src/main/java/games/Choice.java
@@ -2,12 +2,21 @@ package games;
 
 import java.io.IOException;
 
+import static jdk.internal.joptsimple.internal.Strings.LINE_SEPARATOR;
+
 public class Choice {
+
+    static char getCharacterFromUser() throws IOException {
+        byte[] input = new byte[1 + LINE_SEPARATOR.length()];
+        if (System.in.read(input) != input.length)
+            throw new RuntimeException("Пользователь ввёл недостаточное кол-во символов");
+        return (char) input[0];
+    }
 
     public static void main(String[] args) throws IOException {
         System.out.println("Выберите игру:\n1 - \"однорукий бандит\", 2 - \"пьяница\"");
 
-        switch (System.in.read()) {
+        switch (getCharacterFromUser()) {
             case '1': Slot.main(); break;
             case '2': Drunkard.main(); break;
             default: System.out.println("Игры с таким номером нет!");
